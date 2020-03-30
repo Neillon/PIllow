679ae4a2d1366149fddd29c476ee26a92f497c94
Neillon Cesar <neilloncesar13@gmail.com>
2020-03-30 09:49:25 -0300

    Update remote routes

--- data_remote/src/main/java/com/example/data_api/implementations/MovieApiImpl.kt
+++ data_remote/src/main/java/com/example/data_api/implementations/MovieApiImpl.kt
@@ -3,18 +3,17 @@ package com.example.data_api.implementations
 import com.example.domain.entities.Movie
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MovieApiImpl {
 
-    @GET()
+    @GET("trending/movie/week")
     suspend fun listTrendingMovies(): Flow<List<Movie>>
 
-    @GET
-    suspend fun getById(id: Int): Flow<Movie>
+    @GET("/movie/{movie_id}")
+    suspend fun getById(@Path("movie_id") movieId: Int): Flow<Movie>
 
-    @GET
-    suspend fun search(query: String): Flow<List<Movie>>
-
-    @GET
-    suspend fun saveFavoriteMovie(movie: Movie): Flow<Unit>
+    @GET("search/movie?include_adult=false&page=1")
+    suspend fun search(@Query("query") query: String): Flow<List<Movie>>
 }
